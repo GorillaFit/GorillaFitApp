7238d4d1381e01a8c25171e3e0432ac38c6d05cf
jgober96 <jgober96@gmail.com>
2017-06-26 12:11:58 -0700

    API, nutrients, b/l/d, etc. (#14)
    * Staging (#9)
    
    * Update Readme and Schema SQL (#2)
    
    * gitignore issue fixed  (#4)
    
    * Update Readme and Schema SQL
    
    * update gitingore
    
    * created and connected calories, items, and search
    
    * finished search skeleton
    
    * Completed the items component skeleton
    
    * finished total calories skeleton
    
    * added bundle to the git ignore
    
    * attempt 2 to remove bundle.js from commit
    
    * Client Skeleton Created (#7)
    
    * Update Readme and Schema SQL (#2) (#3)
    
    * Fixed an ignore issue  (#5)
    
    * Update Readme and Schema SQL (#2)
    
    * gitignore issue fixed  (#4)
    
    * Update Readme and Schema SQL
    
    * update gitingore
    
    * created and connected calories, items, and search
    
    * finished search skeleton
    
    * Completed the items component skeleton
    
    * finished total calories skeleton
    
    * added bundle to the git ignore
    
    * attempt 2 to remove bundle.js from commit
    
    * client to server get request working
    
    * created first test, and it now passes
    
    * bug fix
    
    * built out test suite
    
    * finished test suite
    
    * few changes
    
    * mvp met with some issue 1) more than one search result, 2) no data persistance, 3) not all promises/axios
    
    * Added breakfast, lunch, dinner, snack options
    
    * about to pull

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request')
 
 var app = express();
 
@@ -7,26 +8,69 @@ var app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json())
 
-app.get('/food', function (req, res) {
+/// DEPRECATED
+
+
+// app.get('/items', function (req, res) {
+//   items.selectAll(function (err, data) {
+//     if (err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.send(data);
+//     }
+//   });
+
+// });
+
+
+
+app.get('/foods', function (req, res) {
   //the below is some code I've added in to verify the client get request works
   //*********************TestCode************************//
-  console.log('this should be the body ', req.url)
-  res
-  .json('this is data from the server')
-  .status(200)
+  //console.log('this should be the body ', req.url)
+
+  //res.send('this is data from the server')
+  //res.status(200)
   //********************TestCode*************************//
+  console.log(req.query.userFood)
 
+  var options = { 
+    method: 'POST',
+    url: 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+    headers: { 
+      'content-type': 'application/json',
+      'x-app-key': '599e301928d15020ff16d7dbeef77f6f',
+      'x-app-id': '9e058c76' 
+    },
+    sort: {
+      order: '_score'
+    },
+    //offset: 0,
+    limit: 5,
 
-  // items.selectAll(function (err, data) {
-  //   if (err) {
-  //     res.sendStatus(500);
-  //   } else {
-  //     res.json(data);
-  //   }
-  // });
-});
+    body: { 
+      query: req.query.userFood,
+      timezone: 'US/Eastern' 
+     },
+     json: true 
+   };
+
+  request(options, function (error, response, body) {
+    if (error) {
+      console.log('here in error')
+	  throw new Error(error);
+	} else {
+	  res.status(200)
+	  console.log(body.foods)
+	  res.send(body.foods)
+	  res.end();
+	  
+	}
+	
+    });
+  });
+
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
-
